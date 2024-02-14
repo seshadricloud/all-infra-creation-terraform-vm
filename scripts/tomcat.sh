2a264c2fecd9bcba5d19cf88c4fcd4ca6806998c
Seshadri Bhavanasi <133082171+seshadricloud@users.noreply.github.com>
2024-02-14 10:50:51 +0530

    Update tomcat.sh

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -6,9 +6,9 @@ sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-ssm-agent
 sudo yum install java -y 
 cd /opt
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.8/bin/apache-tomcat-10.1.8-windows-x64.zip
-unzip apache-tomcat-10.1.8-windows-x64.zip
-rm -rf apache-tomcat-10.1.8-windows-x64.zip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.0-M16/bin/apache-tomcat-11.0.0-M16-windows-x64.zip
+unzip apache-tomcat-11.0.0-M16-windows-x64.zip
+rm -rf apache-tomcat-11.0.0-M16-windows-x64.zip
 cd apache-tomcat-10.1.8/bin/
 chmod 755 *.sh
 sh startup.sh
